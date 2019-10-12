ea216192b9f29aa4f08b32a87399d1ebab3b8657
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-12 02:03:02 -0300

    Finished first function in database init

--- lib/src/services/impl/database_init_impl.dart
+++ lib/src/services/impl/database_init_impl.dart
@@ -1,5 +1,11 @@
 part of storage_fly;
 
+
+
 class DatabaseInitImpl implements DatabaseInit {
 
+  // This method check if the IdbFactory
+  // is supported for your current browser
+  @override
+  bool checkIfDatabaseIsSupported() => IdbFactory.supported;
 }
