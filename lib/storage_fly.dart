f57c7e77ff026df12247fb2804389ec076520156
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-12 02:03:13 -0300

    Changed some storages

--- lib/storage_fly.dart
+++ lib/storage_fly.dart
@@ -1,10 +1,12 @@
 library storage_fly;
 
 // Import native items
-import 'dart:html';
+import 'package:universal_html/prefer_universal/html.dart';
 
 // Import third libary packages
 import 'package:encrypt/encrypt.dart';
+import 'package:universal_html/prefer_universal/indexed_db.dart';
 
 // Make part of project
-part 'src/services/open_database.dart';
+part 'src/services/database_init.dart';
+part 'src/services/impl/database_init_impl.dart';
