43911958321fb4e431f046db421ceedf81ce817d
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-14 23:58:45 -0300

    Added new function to storage fly main class

--- lib/storage_fly.dart
+++ lib/storage_fly.dart
@@ -3,25 +3,18 @@ library storage_fly;
 import 'dart:html' as html;
 import 'dart:indexed_db' as idb;
 
+
+import 'package:storage_fly/src/enums/open_database_enum.dart';
 import 'package:storage_fly/src/models/database_model.dart';
-import 'package:storage_fly/src/services/open_database_service.dart';
+part 'package:storage_fly/src/services/open_database_service.dart';
 
 abstract class StorageFly {
-  
-  // Verify if the current browser
-  // support indexed db into your
-  // current configurations
-  bool isSupported;
 
   Future startDatabase(String databaseName, {int versionNumber});
 
 }
 
 class StorageFlyImpl implements StorageFly {
-  
-  @override
-  bool isSupported = idb.IdbFactory.supported;
-
   @override
   Future startDatabase(String databaseName, {int versionNumber = 1}) =>
         OpenDatabaseService().openDatabase(DatabaseModel(databaseName: databaseName, databaseVersion: versionNumber));
