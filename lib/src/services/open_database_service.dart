9844672fc87b01725885411195b7c278cd04a487
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-14 23:58:30 -0300

    Added open service

--- lib/src/services/open_database_service.dart
+++ lib/src/services/open_database_service.dart
@@ -1,6 +1,20 @@
-import 'package:storage_fly/src/models/database_model.dart';
+part of storage_fly;
 
 class OpenDatabaseService {
-  Future openDatabase(DatabaseModel databaseModel) {
+  Future<OpenDatabaseEnum> openDatabase(DatabaseModel databaseModel) async {
+    if(databaseModel.databaseName.isEmpty) 
+      return OpenDatabaseEnum.DATABASE_NOT_FOUND;
+    if(databaseModel.databaseVersion == null)
+      return OpenDatabaseEnum.DATABASE_VERSION_NOT_FOUND;
+    else
+      try {
+        await html.window.indexedDB.open(
+          databaseModel.databaseName,
+          version: databaseModel.databaseVersion
+        );
+        return OpenDatabaseEnum.OPENED_DATABASE;
+      } catch(e) {
+        return OpenDatabaseEnum.INTERNAL_ERROR;
+      }
   }
 }
